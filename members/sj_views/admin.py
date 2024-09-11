b7c5b206b80f89f1aac87c3680199b2f18f31aad
Matthias Lanz <ansible@squareline.ch>
2024-09-11 21:48:07 +0200

    start authorization for APP-Admin page

--- members/sj_views/admin.py
+++ members/sj_views/admin.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
@@ -13,7 +15,11 @@ from ..sj_utils import get_event_info, sendmail
 
 
 ### administration
-@login_required
+def is_admin(user):
+    return user.groups.filter(name='grp-admin').exists()
+
+@login_required 
+@user_passes_test(is_admin)
 def administration(request):
 
     if request.method == 'POST':
