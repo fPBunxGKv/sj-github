f6bd32332d4dd2b92313881c20faa6f0dee6f707
Matthias Lanz <ansible@squareline.ch>
2025-06-01 21:13:25 +0200

    test docker

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Run database migrations
+echo "Running migrations..."
+python manage.py migrate
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+# Start Gunicorn
+echo "Starting Gunicorn..."
+exec gunicorn --bind 0.0.0.0:8000 --workers 3 dockdjango.wsgi:application
