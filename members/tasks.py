5a4bd88912769c161404a880f18ba998ee7e0c5a
Matthias Lanz <ansible@squareline.ch>
2025-07-31 18:35:54 +0200

    import models

--- members/tasks.py
+++ members/tasks.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-from .models import sj_users
+from members.models import sj_users
 
 logger = logging.getLogger(__name__)
 
