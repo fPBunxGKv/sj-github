4a1e7a41793b1c66c8c8155a58e217ddc3588d5c
Matthias Lanz <ansible@squareline.ch>
2026-08-24 21:41:39 +0200

    add abort button

--- sj/settings.py
+++ sj/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'de-ch'
+LANGUAGE_CODE = 'de-CH'
 
 TIME_ZONE = 'Europe/Zurich'
 
